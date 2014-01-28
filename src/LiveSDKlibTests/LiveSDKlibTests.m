2cb3c13be80a6b398e687d104948da2049397d8a
Constantin Fry <constantine.fry@gmail.com>
2014-01-28 15:02:59 +0100

    Add static library target

---
+++ src/LiveSDKlibTests/LiveSDKlibTests.m
@@ -0,0 +1,34 @@
+//
+//  LiveSDKlibTests.m
+//  LiveSDKlibTests
+//
+//  Created by Constantine Fry on 28/01/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LiveSDKlibTests : XCTestCase
+
+@end
+
+@implementation LiveSDKlibTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
