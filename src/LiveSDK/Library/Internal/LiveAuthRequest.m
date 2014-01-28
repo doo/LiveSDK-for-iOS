5d74a46e01c51a9d062886edd1de25dfa2014e9d
Constantin Fry <constantine.fry@gmail.com>
2014-01-28 15:03:38 +0100

    Fix warning

--- src/LiveSDK/Library/Internal/LiveAuthRequest.m
+++ src/LiveSDK/Library/Internal/LiveAuthRequest.m
@@ -112,7 +112,7 @@ currentViewController:(UIViewController *)currentViewController
         // Adding a checking logic and wait for the modal dialog to appear before we can dismiss it.
         if (self.authViewController.canDismiss) 
         {
-            [self.currentViewController dismissModalViewControllerAnimated:YES];
+            [self.currentViewController dismissViewControllerAnimated:YES completion:nil];
             self.currentViewController = nil;  
             self.authViewController = nil;
         }
@@ -166,8 +166,7 @@ currentViewController:(UIViewController *)currentViewController
     UINavigationController *modalDialog = [[[UINavigationController alloc]initWithRootViewController:self.authViewController]
                                           autorelease];
     
-    [self.currentViewController presentModalViewController:modalDialog 
-                                                  animated:YES];
+    [self.currentViewController presentViewController:modalDialog animated:YES completion:nil];
 }
 
 - (void)retrieveToken
